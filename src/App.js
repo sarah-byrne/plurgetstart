4edae38f6d0e9f2e49a07d092e66a0f772ac832c
sarah-byrne <sarah.byrne@johnlewis.co.uk>
2018-08-31 18:43:35 +0100

    add incomplete code for Working With Data section that I think will be binned for next section anyway

--- src/App.js
+++ src/App.js
@@ -2,49 +2,78 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      counter: 0,
-    }
-  };
 
-  incrementCounter = (incrementValue) => {
-    this.setState( (prevState) => ({
-        counter: prevState.counter + incrementValue
-      }));
+
+
+const Card = (props) => {
+  return (
+    <div style={{margin: '1em'}}>
+        <img width="75" src={props.avatar_url} />
+        <div style={{display: 'inline-block', marginLeft: 10}}>
+          <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+          <div>{props.company}</div>
+        </div>  
+    </div>    
+  );
+};
+
+const CardList = (props) => {
+  return (
+      <div>
+         {props.cards.map(card => <Card {...card}/>)} 
+      </div>    
+  );
+};
+
+class Form extends React.Component {
+  state = { userName: ''}
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Event: Form Submit', this.state.userName);
   };
 
   render() {
     return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          type="text"
+          value={this.state.username}
+          onChange={(event) => this.setState({userName: event.target.value})}
+          placeholder="GitHub username" 
+        />
+        <button type="submit">Add Card</button>
+      </form>  
+    );
+  };
+};
+
+class App extends Component {
+  // constructor(props){
+  //   super(props);
+  // };
+
+  state = {
+    cards: [
+      {name: "Sarah Byrne",
+      avatar_url: "https://avatars0.githubusercontent.com/u/42070896?v=4",
+      company: "JL"},
+      {name: "Sarah M Byrne",
+      avatar_url: "https://avatars0.githubusercontent.com/u/42070896?v=4",
+      company: "JL"},
+    ]
+  };
+  
+  render() {
+    return (
       <div>
-        <Button incrementValue={1} onClickFunction={this.incrementCounter}/>
-        <Button incrementValue={5} onClickFunction={this.incrementCounter}/>
-        <Button incrementValue={10} onClickFunction={this.incrementCounter}/>
-        <Button incrementValue={100} onClickFunction={this.incrementCounter}/>
-        <Result counter={this.state.counter}/>
+        <Form />
+        <CardList cards={this.state.cards} />  
       </div>
     );
   };
 }
 
-class Button extends React.Component {    
-  handleClick = () => {
-    this.props.onClickFunction(this.props.incrementValue);
-  };
-  
-  render() {
-    return (
-      <button onClick={this.handleClick}> + {this.props.incrementValue} </button>
-    );
-  };
-}
 
-const Result = (props) => {
-  return (
-    <div>{props.counter}</div>
-  );
-};
 
 export default App;
